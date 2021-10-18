1470cca2f76cfb1cadc6db0aba84cbd979d09855
DavidLopez89 <iron_lopez89@hotmail.com>
2021-10-18 14:04:08 +0200

    mostrar

--- Program.vb
+++ Program.vb
@@ -5,4 +5,7 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
     End Sub
+    Sub mostrar()
+        Console.WriteLine("Hello World!")
+    End Sub
 End Module
